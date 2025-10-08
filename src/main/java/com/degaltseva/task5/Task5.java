1c4caf5ae2d69570adeae842a9fc337afdb61f23
aldegalts <alinadegalceva@mail.ru>
2025-10-08 17:14:31 +0300

    add task 5

---
+++ src/main/java/com/degaltseva/task5/Task5.java
@@ -0,0 +1,42 @@
+package com.degaltseva.task5;
+
+public class Task5 implements Task {
+    private int seconds;
+    private boolean isRunning;
+    private Thread timerThread;
+
+    public Task5(int seconds) {
+        this.seconds = seconds;
+        this.isRunning = false;
+    }
+
+    @Override
+    public void start() {
+        if (!isRunning) {
+            isRunning = true;
+            timerThread = new Thread(() -> {
+                try {
+                    while (seconds > 0 && isRunning) {
+                        System.out.println("Осталось: " + seconds + " секунд");
+                        Thread.sleep(1000);
+                        seconds--;
+                    }
+                    if (seconds == 0 && isRunning) {
+                        System.out.println("Таймер завершился!");
+                    }
+                } catch (InterruptedException e) {
+                    System.out.println("Таймер был прерван!");
+                }
+            });
+            timerThread.start();
+        }
+    }
+
+    @Override
+    public void stop() {
+        isRunning = false;
+        if (timerThread != null) {
+            timerThread.interrupt();
+        }
+    }
+}
