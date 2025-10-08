fe504cbfcbbaded6ae9a0d59fe4f5b613f605622
aldegalts <alinadegalceva@mail.ru>
2025-10-08 17:14:50 +0300

    add main

--- src/main/java/com/degaltseva/Main.java
+++ src/main/java/com/degaltseva/Main.java
@@ -1,17 +1,35 @@
 package com.degaltseva;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import com.degaltseva.task1.Task1;
+import com.degaltseva.task2.Task2;
+import com.degaltseva.task3.Task3;
+import com.degaltseva.task4.Task4;
+import com.degaltseva.task5.Task5;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Выберите задание (1–5): ");
+        int choice = sc.nextInt();
+
+        switch (choice) {
+            case 1 -> Task1.startTask1();
+            case 2 -> Task2.startTask2();
+            case 3 -> Task3.startTask3();
+            case 4 -> Task4.startTask4();
+            case 5 -> {
+                Task5 timer = new Task5(5);
+                timer.start();
+                try {
+                    Thread.sleep(3000);
+                    timer.stop();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            default -> System.out.println("Неверный выбор.");
         }
     }
 }
